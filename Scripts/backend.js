942b4dcfbd0259a8ed814118fd5d6829f81a6205
Meamrie <lata.p.thakur@gmail.com>
2022-12-07 14:40:23 +0530

    cleancode

---
+++ Scripts/backend.js
@@ -0,0 +1,21 @@
+// Datatypes in computer languages
+var text = "Hello World"; //String
+var number = 42; //Integer
+var char = "@";
+var degree = 50.16; //float
+var isHungry = false //Boolean false or true
+var students = [
+    "Enzo",
+    "Jenny",
+    "Patricia",
+    "Hannatu",
+    "Ivan",
+    "Jessica",
+    "Lata"
+]
+
+console.log("Hello World");
+console.warn(text + " " + degree + " " + isHungry);
+console.info(students[2] + " " + students[6]);
+//Frontend Commands
+alert("The Answer for Everything is ")
---
+++ docs.md
@@ -0,0 +1,11 @@
+# Project Documentation
+### Color Identities
+* 863A6F
+* 975C8D
+* D989B5
+* FFADBC
+### Color Variables
+* --bg:#863A6F;
+* --fg:#D989B5;
+* --pC:#FFADBC;
+* --sC:#975C8D;
